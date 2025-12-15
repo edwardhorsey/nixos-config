a3ff786e0d0418261766f0d60a189e5e566daf55
Ned <wned@proton.me>
2025-12-15 22:06:01 +0000

    Add host to immich

--- adriana.nix
+++ adriana.nix
@@ -70,7 +70,6 @@
 
   services.audiobookshelf = {
     enable = true;
-    user = "ned";
     host = "0.0.0.0";
     port = 13378;
     openFirewall = true;
@@ -78,6 +77,7 @@
 
   services.immich = {
     enable = true;
+    host = "0.0.0.0";
     port = 2283;
     openFirewall = true;
   };
