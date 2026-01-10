7cd6766074281cc6d3a67feb15d45005a016af63
Ned <wned@proton.me>
2026-01-10 09:17:21 +0000

    Add netalertx

--- hosts/dasha.nix
+++ hosts/dasha.nix
@@ -78,6 +78,35 @@
     };
   };
 
+  virtualisation.oci-containers.containers.netalertx = {
+    image = "ghcr.io/jokob-sk/netalertx:latest";
+    autoStart = true;
+    volumes = [
+      "netalertx_data:/data"
+      "/etc/localtime:/etc/localtime:ro"
+    ];
+    environment = {
+      PUID = "20211";
+      PGID = "20211";
+      LISTEN_ADDR = "0.0.0.0";
+      PORT = "20211";
+      GRAPHQL_PORT = "20212";
+      ALWAYS_FRESH_INSTALL = "false";
+      NETALERTX_DEBUG = "0";
+    };
+    extraOptions = [
+      "--network=host"
+      "--cap-drop=ALL"
+      "--cap-add=NET_ADMIN"
+      "--cap-add=NET_RAW"
+      "--cap-add=NET_BIND_SERVICE"
+      "--cap-add=CHOWN"
+      "--cap-add=SETUID"
+      "--cap-add=SETGID"
+      "--read-only"
+    ];
+  };
+
   # Will throw if the dirs don't exist
   systemd.tmpfiles.rules = [
     "d /var/lib/container-data/baikal/config 0755 root root -"
@@ -89,6 +118,8 @@
     3001 # uptime kuma
     8384
     22000
+    20211 # netalertx
+    20212 # netalertx graphql
   ];
 
   networking.firewall.allowedUDPPorts = [
