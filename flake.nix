5c5577adf9322066198fe2e3c64246a4d2c49582
Ned <wned@proton.me>
2026-02-22 10:27:40 +0000

    Add t14 flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "nixos audiobookshelf machine";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
   };
 
@@ -10,31 +11,38 @@
     {
       self,
       nixpkgs,
+      nixpkgsUnstable,
       agenix,
       ...
     }@inputs:
     {
-      nixosConfigurations.adriana = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.adriana = nixpkgsUnstable.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./machines/adriana/adriana.nix
           agenix.nixosModules.default
         ];
-        specialArgs = { inherit inputs; };
       };
 
-      nixosConfigurations.dasha = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.dasha = nixpkgsUnstable.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./machines/dasha/dasha.nix
         ];
       };
 
-      nixosConfigurations.oscar = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.oscar = nixpkgsUnstable.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./machines/oscar/oscar.nix
           agenix.nixosModules.default
+        ];
+      };
+
+      nixosConfigurations.t14 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./machines/t14/t14.nix
         ];
       };
     };
