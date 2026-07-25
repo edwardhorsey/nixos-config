3e45c6c0ff2738f335136e24912dfb7e3c61b1ee
Ned <wned@proton.me>
2026-07-25 12:56:28 +0100

    Update to 26.05

--- machines/t14/t14.nix
+++ machines/t14/t14.nix
@@ -17,7 +17,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "bcachefs" ];
-  boot.kernelPackages = pkgs.linuxPackages_7_0;
+  boot.kernelPackages = pkgs.linuxPackages_7_1;
 
   networking.hostName = "t14";
   networking.networkmanager.enable = true;
