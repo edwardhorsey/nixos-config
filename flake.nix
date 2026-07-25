e8f46b956bf775b62fa8b0f6d294be8a67d5b363
Ned <wned@proton.me>
2026-07-25 12:56:28 +0100

    Update to 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configurations for home infrastructure";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
   };
